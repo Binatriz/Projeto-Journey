567f637c3acce8cc651ae2406786d515afcd8324
Beatriz Cerqueira <beatrizcerqueira008@gmail.com>
2025-11-25 11:47:48 -0300

    Projeto com responsividade e adicionado o menu

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.css']
 })
@@ -15,6 +16,18 @@ export class PerfilComponent {
   userName: string = "Beatriz";
   carLevel: string = "Ford Focus";
 
-  constructor() {}
+  menuOnClick(): void {
+    const menuBar = document.getElementById("menu-bar");
+    const nav = document.getElementById("nav");
+    const menuBg = document.getElementById("menu-bg");
+
+    if (menuBar && nav && menuBg) {
+      menuBar.classList.toggle("change");
+      nav.classList.toggle("change");
+      menuBg.classList.toggle("change-bg");
+    } else {
+      console.warn("Um ou mais elementos não foram encontrados no DOM.");
+    }
+  }
 
 }
