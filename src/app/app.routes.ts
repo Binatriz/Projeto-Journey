c1ab7b90e8ba149c3d0d1d3a7038d8864b394741
Beatriz <beatrizcerqueira008@gmail.com>
2025-12-01 21:48:00 -0300

    Tela de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { CompanheiroFordComponent } from './pages/companheiro-ford/companheiro-ford.component';
 import { MetasComponent } from './pages/metas/metas.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     {path: "", pathMatch: 'full', redirectTo: 'dashboard'},
     {path:'dashboard', component: DashboardComponent},
     {path: 'companheiro-ford', component: CompanheiroFordComponent},
-    {path: 'metas', component: MetasComponent}
+    {path: 'metas', component: MetasComponent},
+    {path: 'login', component: LoginComponent}
 ];
