b1cc50a58731d032cb1f548a2e6845203ff38414
Beatriz <beatrizcerqueira008@gmail.com>
2025-12-04 23:39:21 -0300

    Adicionado responsividade na página Viagens

--- src/app/pages/viagens/viagens.component.ts
+++ src/app/pages/viagens/viagens.component.ts
@@ -247,16 +247,6 @@ São ótimos companheiros para quem quer viajar gastando pouco e com agilidade.
     };
 
     this.sugestao = sugestoes[tipo];
-
-    setTimeout(() => {
-    window.scrollTo({
-      top: document.body.scrollHeight,
-      behavior: 'smooth'
-    });
-  }, 50);
-  setTimeout(() => {
-  document.getElementById('resultado')?.scrollIntoView({ behavior: 'smooth' });
-}, 50);
   }
 
 }
